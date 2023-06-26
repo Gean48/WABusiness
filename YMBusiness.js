1bb864e131bd68995273db9f69586e28a2f95a82
Gean48 <61244326+Gean48@users.noreply.github.com>
2023-06-26 07:38:26 -0300

    Update and rename DeltaBusiness.json to YMBusiness.js

---
+++ YMBusiness.js
@@ -0,0 +1,11 @@
+{
+	"version": "22.2",
+	"download": "https://ymwhatsapp.com/ymwa-business/",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/YMWAB_CHANGELOG.html"
+		}
+
+	]
+}
