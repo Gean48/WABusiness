b2571cbbc115fcb13c79f941b688e7125dd2fcca
Gean48 <61244326+Gean48@users.noreply.github.com>
2023-07-01 23:41:56 -0300

    Update WABusiness.js

--- WABusiness.js
+++ WABusiness.js
@@ -1,5 +1,5 @@
 {
-	"version": "22.2",
+	"version": "30.2",
 	"download": "https://miniencurtafly.blogspot.com/2021/02/gerando-download.html#?o=9080ab1429233eada8e7ef912d89e1be2566630fc6bc4eb4d26e622ec34579a6ecf9d5e44ca5afd8cc9f9a342cc8776246581121fec0e8f075f7a92b8bfc713a497083563df72b89e59aea888f6b44fa333aaec3e8a6ca65",
 	"update": [
 		{
